0a8b599f7251aa8b8a68b5a526346822ceb42a5c
sheyko <sheyko.wk@gmail.com>
2022-06-13 21:11:05 -0500

    add e2e testing auth

--- src/auth/test/integration.test.js
+++ src/auth/test/integration.test.js
@@ -7,6 +7,7 @@ const app = require('../../app').app
 chai.use(chaiHttp)
 
 describe('Suite de testing de integracion para AUTH', () => {
+    // ? Test a ruta protegida
     it('Should return 401 when no jwt available', (done) => {
         chai.request(app)
             .get('/api/v1/post')
@@ -15,10 +16,30 @@ describe('Suite de testing de integracion para AUTH', () => {
                 done()
             })
     })
+    //? Test a login sin data
     it('Should return 400 when no data id provided', (done) => {
         chai.request(app)
             .post('/api/v1/auth/login')
+            .end((err, res) => {
+                chai.assert.equal(res.status, 400)
+                done()
+            })
     })
+    //? Test a login con informacion correcta
+    it('Should reutn 200 when jwt is valid', (done) => {
+        chai.request(app)
+            .post('/api/v1/auth/login')
+            .set("content-type", "application/json")
+            .send({
+                email : "sahid.kick@academlo.com",
+                password : "root"
+            })
+            .end((err, res) => {
+                chai.assert.equal(res.status, 200)
+                chai.assert.typeOf(res.body.token, 'string')
+                done()
+            })
+    } )
 })
 
 
