a7b672ceb4c2243c2bd197a35c8a7661b5e93c40
sheyko <sheyko.wk@gmail.com>
2022-06-13 21:37:53 -0500

    add other e2e testing auth

--- src/auth/test/e2e.test.js
+++ src/auth/test/e2e.test.js
@@ -1,35 +1,107 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-const {describe, it} = require('mocha');
-const app = require('../../app').app
+const { describe, it } = require('mocha');
+const app = require('../../app').app;
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 describe('Suite de test E2E de AUTH', () => {
     it('Should return 200 and token for succesful login', (done) => {
         chai.request(app)
             .post('/api/v1/auth/login')
-                .set("content-type", "application/json")
-                .send({
-                    email : "sahid.kick@academlo.com",
-                    password : "root"
-                })
-                .end((err, res) => {
-                    chai.assert.equal(res.status, 200)
-                    chai.assert.typeOf(res.body.token, 'string')
-                    chai.request(app)
-                        .get('/api/v1/post')
-                        .set("Authorization", `JWT ${res.body.token}`)
-                        .end((err, res) => {
-                            chai.assert.equal(res.status, 200)
-                            done()
-                        })
-                })
-    })
+            .set('content-type', 'application/json')
+            .send({
+                email: 'sahid.kick@academlo.com',
+                password: 'root',
+            })
+            .end((err, res) => {
+                const token = req.body.token;
+                chai.assert.equal(res.status, 200);
+                chai.assert.typeOf(res.body.token, 'string');
+                chai.request(app)
+                    .get('/api/v1/post')
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.assert.equal(res.status, 200);
+                        done();
+                    });
+            });
+    });
+    it('Should return 200 and token for succesful login', (done) => {
+        chai.request(app)
+            .post('/api/v1/auth/login')
+            .set('content-type', 'application/json')
+            .send({
+                email: 'sahid.kick@academlo.com',
+                password: 'root',
+            })
+            .end((err, res) => {
+                const token = req.body.token;
+                chai.assert.equal(res.status, 200);
+                chai.assert.typeOf(res.body.token, 'string');
+                chai.request(app)
+                    .get('/api/v1/post')
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.assert.equal(res.status, 200);
+                        chai.request(app)
+                            .post('/api/v1/post')
+                            .send({
+                                title: 'Test',
+                                content: 'asjfbsjdfbsjdfjd',
+                                user_id: '1',
+                            })
+                            .set('content-type', 'application/json')
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                chai.assert.equal(res.status, 201);
+                                done();
+                            });
+                    });
+            });
+    });
+
+    it('Should return 200 and token for succesful login', (done) => {
+        chai.request(app)
+            .post('/api/v1/auth/login')
+            .set('content-type', 'application/json')
+            .send({
+                email: 'sahid.kick@academlo.com',
+                password: 'root',
+            })
+            .end((err, res) => {
+                const token = req.body.token;
+                chai.assert.equal(res.status, 200);
+                chai.assert.typeOf(res.body.token, 'string');
+                chai.request(app)
+                    .get('/api/v1/post')
+                    .set('Authorization', `JWT ${token}`)
+                    .end((err, res) => {
+                        chai.assert.equal(res.status, 200);
+                        chai.request(app)
+                            .post('/api/v1/post')
+                            .send({
+                                title: 'Test',
+                                content: 'asjfbsjdfbsjdfjd',
+                                user_id: '1',
+                            })
+                            .set('content-type', 'application/json')
+                            .set('Authorization', `JWT ${token}`)
+                            .end((err, res) => {
+                                chai.assert.equal(res.status, 201);
+                                chai.request(app)
+                                    .delete('/api/v1/post/1')
+                                    .set('Authorization', `JWT ${token}`)
+                                    .end((err, res) => {
+                                        chai.assert.equal(res.status, 204);
+                                        done();
+                                    });
+                            });
+                    });
+            });
+    });
+
     // TODO : Crear un test E2E que cree un nuevo post
     // TODO : Crear un test E2E que elimine el post creado
-})
-
-
-
+});
